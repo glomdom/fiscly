3bae7d92513335c315541229af21bcaa364cfdfc
ribamiba <t3omidi010@gmail.com>
2026-03-24 14:47:15 +0200

    login/register and initial api routing

--- frontend/src/routes/register/+page.svelte
+++ frontend/src/routes/register/+page.svelte
@@ -2,7 +2,7 @@
   import { useForm, validators, HintGroup, Hint, email, required } from "svelte-use-form";
   import { onMount } from 'svelte';
   import { fade, fly, slide } from "svelte/transition";
-  import { goto, refreshAll } from '$app/navigation';
+    import { goto } from "$app/navigation";
 
   const form = useForm();
 
@@ -12,12 +12,12 @@
   let ready = true;
   onMount(() => ready = true);
 
-    async function onLogin() {
+  async function onRegister() {
 
     const formData = $form.values; 
     console.log($form.values);
 
-    const response = await fetch('http://10.157.70.56:5096/api/auth/login', {
+    const response = await fetch('http://10.157.70.56:5096/api/auth/register', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData)
@@ -25,35 +25,44 @@
 
 
     if (response.ok) {
-      const data = await response.json();
-      localStorage.setItem("token", data.token);
-
-      goto('/dashboard')
+      goto('/login')
     } else {
-      alert("Couldn't Login.")
+      alert("Error! Couldn't register.")
     }
   }
 </script>
 
-<form use:form on:submit={onLogin}>
+<form use:form on:submit={onRegister}
   <div class="flex flex-auto flex-wrap h-180 bg-linear-to-bl from-slate-950 to-violet-500 pb-8 gap-4 content-center place-content-end pr-100">
     {#if ready}
 
       <div class="text-left text-gray-50 text-6xl font-bold pt-50 mr-50" transition:fade={{duration:1000}}>
-        Welcome back!
+        Make your account <br> today, for free!
       </div>
 
-      <div class="rounded-4xl flex-col flex flex-wrap w-100 h-130 bg-gray-50 pb-4 gap-2 content-center place-content-center"
+      <div class="rounded-4xl flex-col flex flex-wrap w-100 h-160 bg-gray-50 pb-4 gap-2 content-center place-content-center"
       transition:fly={{y:100, duration:600}}>
         <div class="text-4xl pb-4 text-center">
-          Login
+          Register
         </div>
 
-            <!--   LOGIN VALUES   -->
+            <!--   REGISTER VALUES   -->
 
         <hr class="gray-200">
+        <h1 class="text-2xl text-gray-500">First Name:</h1>
+          
+        <input type="firstName" name="firstName" use:validators={[required]}
+        class="w-75 h-10 opacity-75 rounded-sm border border-black bg-gray-50 hover:bg-gray-200 text-1xl text-gray-950 font-medium pl-2"/>
+        <Hint for="firstName" on="required">Name required</Hint>
+
+        <h1 class="text-2xl text-gray-500">Last Name:</h1>
+          
+        <input type="lastName" name="lastName" use:validators={[required]}
+        class="w-75 h-10 opacity-75 rounded-sm border border-black bg-gray-50 hover:bg-gray-200 text-1xl text-gray-950 font-medium pl-2"/>
+        <Hint for="lastName" on="required">Name required</Hint>
+
         <h1 class="text-2xl text-gray-500">Email:</h1>
-        
+
         <input type="email" name="email" use:validators={[required, email]}
         class="w-75 h-10 opacity-75 rounded-sm border border-black bg-gray-50 hover:bg-gray-200 text-1xl text-gray-950 font-medium pl-2"/>
 
@@ -68,20 +77,13 @@
         class="w-75 h-10 opacity-75 rounded-sm border border-black bg-gray-50 hover:bg-gray-200 text-1xl text-gray-950 font-medium pl-2"/>
         <Hint for="password" on="required">This is a mandatory field</Hint>
 
-
         
-        <div class="flex flex-col pt-10 pl-13">
+        <div class="pt-10 pl-13">
           <button disabled={!$form.valid}
-          class="w-50 h-20 opacity-75 rounded-2xl bg-indigo-700 hover:bg-fuchsia-700 hover:underline text-3xl font-medium text-gray-50">Login</button>
-
+          class="w-50 h-20 opacity-75 rounded-2xl bg-indigo-700 hover:bg-fuchsia-700 hover:underline text-3xl font-medium text-gray-50">Register</button>
         </div>
-
-        
-           <a href="/register/" class="self-center text-gray-700 underline hover:text-fuchsia-700">Don't have an account? Register</a>
       </div>
     {/if}
-  </div>
-
 </form>
 
 <style>
