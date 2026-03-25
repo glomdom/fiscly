79f4265bc93edc7fcf06bdccc15b85d6f1615c5c
glomdom <103685817+glomdom@users.noreply.github.com>
2026-03-25 11:32:45 +0200

    feat: null progate models

--- backend/Fiscly/Fiscly.Api/Domain/Transaction.cs
+++ backend/Fiscly/Fiscly.Api/Domain/Transaction.cs
@@ -4,8 +4,8 @@ public sealed class Transaction {
     public Guid Id { get; set; }
     public Guid UserId { get; set; }
 
-    public string Merchant { get; set; }
-    public string Category { get; set; }
+    public string Merchant { get; set; } = null!;
+    public string Category { get; set; } = null!;
     public decimal Amount { get; set; }
     public DateTime Date { get; set; }
 }
