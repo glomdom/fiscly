5156e5827fb4289b5b685eaceb60ba5a2ae0a21e
glomdom <103685817+glomdom@users.noreply.github.com>
2026-03-25 11:09:48 +0200

    fix: fart

--- frontend/src/routes/dashboard/+page.svelte
+++ frontend/src/routes/dashboard/+page.svelte
@@ -16,7 +16,7 @@
         <div
           class="w-14 h-14 bg-linear-to-br from-indigo-500 to-fuchsia-500 rounded-2xl flex items-center justify-center text-3xl font-bold text-white shadow-lg shadow-indigo-500/20"
         >
-          M
+          F
         </div>
         <div>
           <h1 class="text-4xl font-bold tracking-tight">Dashboard</h1>
