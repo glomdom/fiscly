3ad988ee742546621dfd4e96ec78833923f69c1e
glomdom <103685817+glomdom@users.noreply.github.com>
2026-03-30 17:39:54 +0300

    chore: dont use upcoming bills

--- frontend/src/routes/dashboard/+page.svelte
+++ frontend/src/routes/dashboard/+page.svelte
@@ -58,7 +58,7 @@
     </div>
 
     <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
-      <div class="lg:col-span-2 bg-slate-900/60 rounded-3xl p-8 border border-white/5 shadow-sm flex flex-col min-h-85">
+      <div class="lg:col-span-3 bg-slate-900/60 rounded-3xl p-8 border border-white/5 shadow-sm flex flex-col min-h-85">
         <h3 class="text-sm font-semibold mb-8 text-slate-400 uppercase tracking-wider">Spending Breakdown</h3>
 
         <div class="space-y-6 grow flex flex-col justify-center">
@@ -104,7 +104,7 @@
         </div>
       </div>
 
-      <UpcomingBills />
+      <!-- <UpcomingBills /> -->
 
       <div class="lg:col-span-3">
         {#await data.streamed.transactions}
